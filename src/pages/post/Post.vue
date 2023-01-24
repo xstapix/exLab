3c84c9fadcd61177cf07146cd536b940282819df
xstapix <sarafannikov.85@mail.ru>
2023-01-24 21:47:46 +0300

    fix post page

--- src/pages/post/Post.vue
+++ src/pages/post/Post.vue
@@ -1,7 +1,7 @@
 <script setup>
   import {reactive} from 'vue'
   import { useRoute } from 'vue-router'
-  import {useStore} from '@/stores/postStore'
+  import {getMethods} from '@/methods.js'
 
   import SideBar from '@/components/sideBar/SideBar.vue'
   import Carousel from '@/components/swiper/Carousel.vue'
@@ -10,21 +10,39 @@
   import './media-style.css'
   
   const route = useRoute()
-  const postsStore = useStore()
+  const useMethod = getMethods()
 
   document.body.style.backgroundColor = '#E5E5E5'
   
   const state = reactive({
-    postViewed: postsStore.data.posts[route.params.id],
+    postViewed: {},
+    renderKey: 1,
+    amountPost: 0
   })
 
+  setState()
+
+  async function setState() {
+    const allPosts = await useMethod.getPosts()
+    state.postViewed = allPosts[route.params.id]
+    state.amountPost = allPosts.length
+  }
+  
+  const re_render = () => {
+    state.renderKey++
+    setState()
+}
 </script>
 
 <template>
-  <div class="DF">
+  <div class="DF" :key="renderKey">
     <SideBar/>
-    <router-view :key="route.fullPath" />
-    <router-link :to="`/post/${state.postViewed.id - 1 }`" :key="route.fullPath" class="postPrew"></router-link>
+    <router-link :to="`/post/${state.postViewed.id - 1 }`" 
+      @click="re_render" 
+      class="postPrew"
+      v-if="route.params.id != 0">
+    </router-link>
+    <div v-else></div>
     <div class="single_post-body">
       <div class="single_post-content">
         <p class="single_post-content_title">{{ state.postViewed.title }}</p>
@@ -32,6 +50,11 @@
       </div>
       <Carousel :activePost="state.postViewed"/>
     </div>
-    <router-link :to="`/post/${state.postViewed.id + 1 }`" class="postNext"></router-link>
+    <router-link :to="`/post/${state.postViewed.id + 1 }`" 
+      @click="re_render" 
+      class="postNext"
+      v-if="route.params.id != state.amountPost - 1">
+    </router-link>
+    <div v-else></div>
   </div>
 </template>
